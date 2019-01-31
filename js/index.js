c54505fd76767a556c4a42c1f989d571c2c40dc6
Claire Sinozich <claire.sinozich@gmail.com>
2019-01-30 20:00:03 -0700

    Mostly Everything

--- js/index.js
+++ js/index.js
@@ -41,8 +41,63 @@ const siteContent = {
 let logo = document.getElementById("logo-img");
 logo.setAttribute('src', siteContent["nav"]["img-src"])
 
-let callToAction = document.getElementById("cta-img");
-callToAction.setAttribute('src', siteContent["cta"] ["img-src"])
+let ctaImage = document.getElementById("cta-img");
+ctaImage.setAttribute('src', siteContent["cta"] ["img-src"])
 
 let middleImage = document.getElementById("middle-img");
 middleImage.setAttribute('src', siteContent["main-content"] ["middle-img-src"]);
+
+//==============
+
+let navItems = document.querySelectorAll('a');
+navItems[0].textContent = siteContent["nav"]["nav-item-1"];
+navItems[1].textContent = siteContent["nav"]["nav-item-2"];
+navItems[2].textContent = siteContent["nav"]["nav-item-3"];
+navItems[3].textContent = siteContent["nav"]["nav-item-4"];
+navItems[4].textContent = siteContent["nav"]["nav-item-5"];
+navItems[5].textContent = siteContent["nav"]["nav-item-6"];
+navItems[0, 1, 2, 3, 4, 5].style.color = "#b0b0b0";
+
+
+let ctaHeader = document.querySelector("h1");
+ctaHeader.textContent = siteContent["cta"]["h1"];
+ctaHeader.style.fontSize = "72px"
+ctaHeader.style.textAlign = "center"
+ctaHeader.style.spacing = "5px"
+ctaHeader.style.justifyContent = "center";
+ctaHeader.style.flexFlow = "column";
+ctaHeader.style.alignItems = "center"
+ctaHeader.style.width = "170px";
+ctaHeader.style.margin = "0 82px 0 0"; //Fix ctaHeader centering
+
+let ctaButton = document.querySelector("button");
+ctaButton.textContent = siteContent["cta"]["button"];
+ctaButton.style.border = "1px solid black";
+ctaButton.style.width = "170px";
+ctaButton.style.height = "32px";
+ctaButton.style.fontSize = "16px";
+ctaButton.style.marginTop = "10px";
+ctaButton.style.cursor = "pointer";
+
+let mainContentHeaders = document.querySelectorAll("h4");
+mainContentHeaders[0].textContent = siteContent["main-content"]["features-h4"];
+mainContentHeaders[1].textContent = siteContent["main-content"]["about-h4"];
+mainContentHeaders[2].textContent = siteContent["main-content"]["services-h4"];
+mainContentHeaders[3].textContent = siteContent["main-content"]["product-h4"];
+mainContentHeaders[4].textContent = siteContent["main-content"]["vision-h4"];
+mainContentHeaders[5].textContent = siteContent["contact"]["contact-h4"];
+
+let paragraphs = document.querySelectorAll("p");
+paragraphs[0].textContent = siteContent["main-content"]["features-content"];
+paragraphs[1].textContent = siteContent["main-content"]["about-content"];
+paragraphs[2].textContent = siteContent["main-content"]["services-content"];
+paragraphs[3].textContent = siteContent["main-content"]["product-content"];
+paragraphs[4].textContent = siteContent["main-content"]["vision-content"];
+paragraphs[5].textContent = siteContent["contact"]["address"];
+paragraphs[6].textContent = siteContent["contact"]["phone"];
+paragraphs[7].textContent = siteContent["contact"]["email"];
+paragraphs[7].textContent = siteContent["footer"]["copyright"];
+
+let footer = document.getElementsByTagName("footer");
+footer[0].style.textAlign = "center";
+footer[0].style.margin = "40px 0 20px";
